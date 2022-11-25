692b36495e73073c31f4c489613f80e0f695fa6d
GaBaljaintheroom <junsu1222@naver.com>
2022-11-25 13:12:46 +0900

    2.3.1 Create Todo 구현

--- src/main/java/com/tukoreatp/wepstudying/service/TodoService.java
+++ src/main/java/com/tukoreatp/wepstudying/service/TodoService.java
@@ -1,10 +1,14 @@
 package com.tukoreatp.wepstudying.service;
 
+import lombok.extern.slf4j.Slf4j;
 import com.tukoreatp.wepstudying.model.TodoEntity;
 import com.tukoreatp.wepstudying.persistence.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -19,5 +23,28 @@ public class TodoService {
         //TodoEntity 검색
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        //Validations
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity id : {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    private static void validate(TodoEntity entity) {
+        if(entity == null){
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+
+        if(entity.getUserId() == null){
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
     }
 }
